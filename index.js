56b9b86ddf0cf715e2417d0c91862cc1976a9ead
Peter Ajtai <peter.ajtai@thinksolid.com>
2016-02-02 09:00:29 -0800

    validation rules

--- index.js
+++ index.js
@@ -4,12 +4,110 @@ var _h = require('highland'),
     _ = require('lodash'),
     getSetter = require('get-setter');
 
-function Model(initialData, rules) {
+// Click through to the methods for comments
+Model.prototype.set = set;
+Model.prototype.get = get;
+Model.prototype.getState = getState;
+Model.prototype.rules = rules;
+Model.prototype.validate = validate;
+
+module.exports = Model;
+
+function Model(initialData) {
     if (!(this instanceof Model)) {
         return new Model(initialData, rules);
     }
 
+    this._data = initialData || {};
+    this._rules = {};
+    this._meta = {
+        validation : {
+            validated : false,
+            valid : false,
+            errors : []
+        }
+    };
+
+    this._lastSet = '';
+
     this.stream = _h();
+    this.stream.write(this.getState());
+
+    // just being explicit
+    return this;
 }
 
-module.exports = Model;
+/**
+ *
+ * @param key
+ * @param value
+ * @returns {set}
+ */
+function set(key, value) {
+    getSetter.set.call(this._data, key, value);
+    return this;
+}
+
+function get(key) {
+    return getSetter.get.call(this._data, key);
+}
+
+/**
+ * Gets the current data and meta data.
+ * The metadata inclused all filterable properties.
+ * The data is the raw data object.
+ * @returns {{}}
+ */
+function getState() {
+    return {
+        set : this._lastSet,
+        meta : this._meta,
+        data : this._data
+    };
+}
+
+/**
+ * Define the validation rules as an object.
+ * The keys are the dot notation getters
+ * The values are callbacks that will get called with the model value. They should return and error string if there is
+ * an error, and they should return falsey if not.
+ * When the rules are called, they are called with the model as context.
+ * @param ruleSet
+ * @returns {rules}
+ */
+function rules(ruleSet) {
+    _.extend(this._rules, ruleSet);
+    return this;
+}
+
+/**
+ * Will run the rules that were passed in via model.rules.
+ * Listen to model.stream for updates.
+ * @returns {validate}
+ */
+function validate() {
+    var errors = [],
+        self = this;
+
+    _.forEach(this._rules, function(rule, key) {
+        var errorString = rule.call(self, self.get(key));
+        if (errorString && errorString.length) {
+            errors.push([key, errorString]);
+        }
+    });
+
+    if (errors.length) {
+        this._meta.validation = {
+            validated : true,
+            valid : false,
+            errors : errors
+        }
+    } else {
+        this._meta.validation = {
+            validated: false,
+            valid: true,
+            errors: []
+        }
+    }
+    return this;
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -8,6 +8,15 @@ chai.should();
 
 describe('model stream', function() {
     describe('set', function() {
+        describe('simple non stream', function() {
+            it('should be able to set and get simply using dot notation', function() {
+                var model = Model();
+
+                model.set('a.b.c', 'alphabet');
+
+                model.get('a.b.c').should.equal('alphabet');
+            });
+        });
 
         it('initialization adds to the stream', function(done) {
             var stream = Model({
@@ -20,14 +29,64 @@ describe('model stream', function() {
                         a : 1
                     },
                     set : '',
-                    valid : {
-                        validated : false,
-                        valid : true,
-                        errors : []
+                    meta : {
+                        validation: {
+                            validated: false,
+                            valid: false,
+                            errors: []
+                        }
                     }
                 });
                 done();
             });
+        });
+
+        xit('should set vaildated to false after any set');
+        xit('should be able to set multiple values by passing in an array');
+        xit('should be able to skip validation by setting options.validate to false');
+    });
+
+    describe('validate', function() {
+        it('should be able to set validation rules for keys that return an error string if not valid', function(done) {
+            var model = new Model({
+                user : {
+                    name : {
+                        first : '',
+                        last : ''
+                    }
+                }
+            });
+
+            model.rules({
+                'user.name.first' : validateName
+            });
+
+            function validateName(name) {
+                // name cannot begin with number
+                if (/^\d/.test(name)) {
+                    return 'Sorry, numbers are not allowed as the first character of a name.';
+                }
+            }
+
+            model.set('user.name.first', '8 too much, Sr. III Esquire');
+            model.validate();
+
+            model.stream.pull(function(err, state) {
+                state.meta.validation.valid.should.equal(false);
+                state.meta.validation.errors.should.deep.equal([
+                    ['user.name.first', 'Sorry, numbers are not allowed as the first character of a name.']
+                ]);
+                done();
+            });
+        });
+
+        xit('should be able to validate collections (arrays)');
+        xit('should be able to support async validations via streams');
+    });
+
+    describe('examples', function() {
+        describe('validation', function() {
+
         });
     });
 });
